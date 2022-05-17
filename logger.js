801ab2125f28154b6f04259311fcd6aedfa2c8a1
Cameron Bourque <28263454+CameronBourque@users.noreply.github.com>
2022-05-17 16:33:12 -0500

    updated logger for consistency

--- logger.js
+++ logger.js
@@ -1,3 +1,12 @@
 export async function logRoleIdentified(member, role) {
     console.log(member.member.user.username + ' has the role "' + role + '". Joining their channel!');
 }
+
+// These were made for code consistency's sake
+export async function logDebug(msg) {
+    console.log(msg);
+}
+
+export async function logError(error) {
+    console.error(error);
+}
