420714c9c9b2e4577b636d74f5558896a44933e7
Cameron Bourque <28263454+CameronBourque@users.noreply.github.com>
2022-05-17 13:48:27 -0500

    seems concurrency has been fixed by discord.js so time to move to multiserver support

--- audio.js
+++ audio.js
@@ -1,6 +1,8 @@
 export async function playSound (userChannel, sound) {
     userChannel.join().then(connection => {
+        //TODO: Replace this with getting soundfile from storage/db
         const soundFile = require("path").join(__dirname, sound);
+
         const dispatcher = connection.play(soundFile, {volume: 1.0});
 
         dispatcher.on('finish', () => {
