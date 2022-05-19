cf2ff609d3f89eb358225e6d8eac399f3531b37d
Cameron Bourque <28263454+CameronBourque@users.noreply.github.com>
2022-05-19 10:00:59 -0500

    Logger no longer async

--- logger.js
+++ logger.js
@@ -1,12 +1,12 @@
-export async function logRoleIdentified(member, role) {
+export function logRoleIdentified(member, role) {
     console.log(member.member.user.username + ' has the role "' + role + '". Joining their channel!');
 }
 
 // These were made for code consistency's sake
-export async function logDebug(msg) {
+export function logDebug(msg) {
     console.log(msg);
 }
 
-export async function logError(error) {
+export function logError(error) {
     console.error(error);
 }
