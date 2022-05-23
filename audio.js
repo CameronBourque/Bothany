9d07cf147900598c413daae0befb3b80f01f8950
Cameron Bourque <28263454+CameronBourque@users.noreply.github.com>
2022-05-23 12:07:36 -0500

    Rework to sound playing with new discord voice package

--- audio.js
+++ audio.js
@@ -1,21 +1,47 @@
 import path from 'path';
 import {fileURLToPath} from "url";
+import {
+    AudioPlayerStatus,
+    createAudioPlayer,
+    createAudioResource,
+    joinVoiceChannel,
+    VoiceConnectionStatus
+} from "@discordjs/voice";
 
 // Fix dirname since we can't use require
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 export async function playSound (userChannel, sound) {
-    userChannel.join().then(connection => {
-        //TODO: Replace this with getting soundfile from storage/db
-        const soundFile = path.join(__dirname, sound);
+    const connection = joinVoiceChannel({
+        channelId: userChannel.id,
+        guildId: userChannel.guildId,
+        adapterCreator: userChannel.guild.voiceAdapterCreator
+    })
 
-        const dispatcher = connection.play(soundFile, {volume: 1.0});
+    const audioPlayer = createAudioPlayer()
 
-        dispatcher.on('finish', () => {
-            userChannel.leave();
-        });
-    }).catch(function(error) {
-        console.log('Unable to join the channel (no permission?)');
-    });
+    const subscription = connection.subscribe(audioPlayer)
+
+    // TODO: Remove once we are able to retrieve and play the sound
+    const soundFile = path.join(__dirname, 'sounds/afk.mp3');
+
+    if(subscription) {
+        connection.on(VoiceConnectionStatus.Ready, () => {
+            // Play the sound
+            audioPlayer.play(createAudioResource(soundFile))
+
+            audioPlayer.on(AudioPlayerStatus.Idle, () => {
+                audioPlayer.stop()
+                connection.destroy()
+            })
+        })
+
+        connection.on(VoiceConnectionStatus.Disconnected, () => {
+            // Make sure it gets destroyed
+            audioPlayer.stop()
+            connection.destroy()
+        })
+    }
+
 }
